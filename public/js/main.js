cc836b59af0d37f7c0e11d3cd6f3eeea9bfba908
bt <btfranzen@gmail.com>
2017-12-13 20:31:36 -0700

    responsive fixes

--- public/js/main.js
+++ public/js/main.js
@@ -2,8 +2,7 @@ console.log('js is linked, yo')
 
 Vue.component('main-nav', {
   template: `
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <a class="navbar-brand" href="/" id="brand"> TriggerAware </a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -19,7 +18,6 @@ Vue.component('main-nav', {
           <li class="nav-item">
             <a class="nav-link" href="/recommend"> recommend a triggering scene </a>
           </li>
-
         </ul>
       </div>
     </nav>
